eaaf88225ff69b48ddd0990fa8800f5f475a82e7
RalatPC <ralatrado@gmail.com>
2023-05-02 22:59:31 +0100

    event listener for input and validate regex

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^[0-9]{11}$/,
+};
+
+function validate(field, regex) {
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
