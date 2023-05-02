12370af6d2759c7b4afeb7f39c467fce375f8bdc
RalatPC <ralatrado@gmail.com>
2023-05-02 23:54:34 +0100

    added all regex for all inputs

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
+    username: /^[a-z0-9]{5,12}$/i,
     telephone: /^[0-9]{11}$/,
+    password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/,
+    slug: /^[a-z0-9-]{4,10}$/i,
+    email:  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 };
 
 function validate(field, regex) {
